7fd660dab5917b243a3bfff3856267e98bef5791
Diem <Diem@PigPull>
2015-05-23 14:32:10 +0700

    example03

---
+++ example03/src/Rectangle.java
@@ -0,0 +1,15 @@
+
+public class Rectangle {
+	int width,height;
+	public Rectangle(int a,int b){
+		width=a;
+		height=b;
+	}
+	public Rectangle(){
+		width=5;
+		height=5;
+	}
+	public int area(){
+		return (width*height);
+	}
+}
