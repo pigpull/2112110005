b06937ffbe5831baecf0657c765bb7d1ee77f38a
Diem <Diem@PigPull>
2015-05-23 14:35:07 +0700

    example07

---
+++ example07/src/Polygon.java
@@ -0,0 +1,12 @@
+
+public class Polygon {
+	protected int width,height;
+	public Polygon(int a){
+		width=a;
+		
+	}
+	public void setValues(int a,int b){
+		width=a;
+		height=b;
+	}
+}
---
+++ example07/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle rect = new Rectangle(8, 9);
+		System.out.println("rect a:{0}"+rect.area());
+		return;
+	}
+
+}
