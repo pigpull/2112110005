3968ce44c8185cbbfe41a9cdc7530378aa92f7f7
Diem <Diem@PigPull>
2015-05-23 14:34:41 +0700

    example06

---
+++ example06/src/Test.java
@@ -0,0 +1,16 @@
+import java.io.Console;
+
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle rect=new Rectangle(8,9);		
+		System.out.println("rect a:{0}"+rect.area());		
+		return;
+	}
+
+}
