5fe2b2ec75d17b9d0c81fab49d45e0ff883b6463
Diem <Diem@PigPull>
2015-05-23 14:32:52 +0700

    example04

---
+++ example04/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle rect=new Rectangle(3, 4);
+		Rectangle rectb=new Rectangle(5, 6);
+		rectb=rect;
+		System.out.println("rect a:"+rect.area());
+		System.out.println("rect b:"+rectb.area());
+	}
+
+}
